26229aca68c59d19589e1c461e18f240c6b869cf
richardsondev <contact@richardson.dev>
2025-02-25 07:03:13 -0800

    [WIP] Support number literals and variable references

--- compiler/Lexer/Token.cs
+++ compiler/Lexer/Token.cs
@@ -3,12 +3,41 @@
 public enum TokenType
 {
     AT, IDENT, EQUAL, GETENV, LPAREN, RPAREN, LBRACE, RBRACE,
-    STRING, PLUS, IF, ELSE, PRINT, EQEQ, EOF, GETWEB, POSTWEB, STARTSERVER, EXIT
+    STRING, NUMBER, PLUS, IF, ELSE, PRINT, EQEQ, EOF,
+    GETWEB, POSTWEB, STARTSERVER, EXIT
+}
+
+public enum TokenKind
+{
+    TEXT, NUMBER
 }
 
 public class Token
 {
     public TokenType Type { get; }
-    public string Text { get; }
-    public Token(TokenType type, string text) { Type = type; Text = text; }
+    public TokenKind Kind { get; }
+    private readonly string? text;
+    private readonly long? number;
+
+    public Token(TokenType type, string text)
+    {
+        Type = type;
+        Kind = TokenKind.TEXT;
+        this.text = text;
+    }
+
+    public Token(TokenType type, long number)
+    {
+        Type = type;
+        Kind = TokenKind.NUMBER;
+        this.number = number;
+    }
+
+    public string Text => Kind == TokenKind.TEXT
+        ? text!
+        : throw new InvalidOperationException($"Attempted to access Text on a {Kind} token.");
+
+    public long Number => Kind == TokenKind.NUMBER
+        ? number!.Value
+        : throw new InvalidOperationException($"Attempted to access Number on a {Kind} token.");
 }
